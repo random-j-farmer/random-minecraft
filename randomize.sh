f3c548c99b07b731772180babd59c3685b3c3ecf
Random J. Farmer <random.j.farmer@gmail.com>
2020-12-27 23:37:39 +0100

    randomize more!

--- randomize.sh
+++ randomize.sh
@@ -6,6 +6,8 @@
 git filter-branch -f --commit-filter '
    GIT_AUTHOR_NAME="Random J. Farmer";
    GIT_AUTHOR_EMAIL="random.j.farmer@gmail.com";
+   GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME";
+   GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL";
    git commit-tree "$@";
 ' HEAD
 
